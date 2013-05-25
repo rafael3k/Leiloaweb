1cbbb9b5722338db9ea9f56f0a0144b5d115c2c2
Vitor Raphael Machado Costa <vitor.rmachado@yahoo.com.br>
2013-05-24 23:14:29 -0300

    Versão inicial do projeto

---
+++ leiloaweb/src/br/com/leilao/controller/TesteController.java
@@ -0,0 +1,19 @@
+package br.com.leilao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TesteController {
+	
+	@RequestMapping("teste")
+	public ModelAndView testaSpring(){
+		ModelAndView view = new ModelAndView();
+		System.out.println("TESTANDO SPRING");
+		view.setViewName("site/resposta");
+		view.addObject("resposta", "RESPOSTA QUE O SPRING DEVOLVE PARA A VIEW");
+		return view;
+	}
+
+}
